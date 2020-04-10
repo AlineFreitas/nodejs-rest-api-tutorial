1ce7c1abbcbd035bccd07fa5763ee7fe0bbec1c8
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-10 18:40:00 -0300

    Uses express to start the api on port 3333

--- start.js
+++ start.js
@@ -1,2 +1,9 @@
 'use strict'
 
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+console.log(`Starting app on port ${port}`);
+app.listen(port);
