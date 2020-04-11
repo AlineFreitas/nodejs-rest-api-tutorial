af517b7299a7ce8b85d4c5fb988717baf45ff8d0
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-11 02:13:10 -0300

    Creates route to list products by tag

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,6 +3,7 @@ const productController = require('../controllers/products_controller.js');
 
 router.get('/', productController.list);
 router.get('/:slug', productController.getBySlug);
+router.get('/tags/:tag', productController.getByTag);
 router.post('/', productController.create);
 
 module.exports= router;
