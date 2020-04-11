a8b645ffc92d7d78640b3d4c47e0a3d7cbfaf5c4
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-10 23:23:01 -0300

    Modifies src/bin/app.js to interpret json on request body

--- src/bin/app.js
+++ src/bin/app.js
@@ -1,6 +1,9 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 const routes = require('../routes');
+
+app.use(express.json())
 
 app.use(routes);
 
