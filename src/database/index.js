46c06da4a5efdf26a498712f5d88d38c21c05521
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-11 09:57:25 -0300

    Adds useUnifiedTopology: true option to mongoose.connect on src/database/index.js

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
   'mongodb://localhost:27017/product-api-rest',
-  {useNewUrlParser: true}
+  {useNewUrlParser: true, useUnifiedTopology: true}
 );
 
 mongoose.Promise = global.Promise;
