d500e4ce5ddb03101feb58290640e08af570f9c7
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-11 02:58:40 -0300

    WIP Adds route to PUT /products/:id

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,6 +4,9 @@ const productController = require('../controllers/products_controller.js');
 router.get('/', productController.list);
 router.get('/:slug', productController.getBySlug);
 router.get('/tags/:tag', productController.getByTag);
+
 router.post('/', productController.create);
+
+router.put('/:id', productController.update);
 
 module.exports= router;
