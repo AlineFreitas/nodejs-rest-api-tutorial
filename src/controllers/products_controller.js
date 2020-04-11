c1c9b07b526b015245002aa5dc785d1f66865ad9
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-10 23:25:48 -0300

    Add POST /products endpoint and maps it in the controller

--- src/controllers/products_controller.js
+++ src/controllers/products_controller.js
@@ -1,5 +1,8 @@
 module.exports = {
   list(request, response){
     response.json(request.body);
+  },
+  async create(request, response){
+    return response.json(request.body);
   }
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const productController = require('../controllers/products_controller.js');
 
 router.get('/', productController.list);
+router.post('/', productController.create);
 
 module.exports= router;
